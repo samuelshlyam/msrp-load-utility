18a056c5bebf865335492f78375520bb63e38345
nikiconluxury <149531476+nikiconluxury@users.noreply.github.com>
2024-07-16 18:54:22 -0400

    Update Vendor_load.py

--- Vendor_load.py
+++ Vendor_load.py
@@ -14,7 +14,7 @@ engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % conn)
 
 filetype = input('''
 Upload ready for:  
-''')
+''').lower()
 
 setupid = 0
 
@@ -35,7 +35,7 @@ if filetype.find('bottega') > 0:
     filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27']
 if filetype.find('ysl') > 0:
     setupid = 478
-    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27']
+    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27','F28','F29','F30','F31']
 if filetype.find('balenciaga') > 0:
     setupid = 66
     filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27']
@@ -45,7 +45,7 @@ if filetype.find('versace') > 0:
                    'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20']
 if filetype.find('ferragamo') > 0:
     setupid = 481
-    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7']
+    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7','F8']
 if filetype.find('balmain') > 0:
     setupid = 68
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13',
@@ -61,7 +61,7 @@ if filetype.find('givenchy') > 0:
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9']
 if filetype.find('mccartney') > 0:
     setupid = 498
-    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9']
+    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9','F10']
 if filetype.find('etro') > 0:
     setupid = 187
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7']
@@ -79,10 +79,10 @@ if filetype.find('bally') > 0:
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9','F10']
 if filetype.find('goldengoose') > 0:
     setupid = 228
-    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7']
+    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7','F8']
 if filetype.find('kenzo') > 0:
     setupid = 275
-    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9','F10', 'F11']
+    filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8','F9','F10', 'F11','F12']
 if filetype.find('canada_goose') > 0:
     setupid = 110
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6']
@@ -92,6 +92,16 @@ if filetype.find('dsquared') > 0:
 if filetype.find('mcm') > 0:
     setupid = 343
     filecolumns = ['BrandID', 'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7','F8']
+if filetype.find('loewe') > 0:
+    setupid = 310
+    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27','F28','F29','F30','F31','F32','F33','F34','F35','F36','F37','F38']
+if filetype.find('celine') > 0:
+    setupid = 118
+    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7']
+if filetype.find('miu_miu') > 0:
+    setupid = 358
+    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7']
+
 
 if setupid > 0 :
 
