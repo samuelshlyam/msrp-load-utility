d979fc39836792f65f3a12c8e3fc9225bdc2c762
Sam <sammich5302@gmail.com>
2024-07-17 11:37:08 -0400

    Loading for Jacquemus

--- Vendor_load.py
+++ Vendor_load.py
@@ -115,7 +115,9 @@ if filetype.find('marni') > 0:
 if filetype.find('loro') > 0:
     setupid = 314
     filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11','F12','F13','F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21','F22','F23','F24','F25','F26','F27','F28','F29','F30','F31','F32','F33','F34','F35','F36']
-
+if filetype.find('jacquemus') > 0:
+    setupid = 263
+    filecolumns = ['BrandID', 'F0', 'F1','F2', 'F3', 'F4', 'F5', 'F6', 'F7','F8','F9']
 
 if setupid > 0 :
 
